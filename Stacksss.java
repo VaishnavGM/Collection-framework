02afcec657b2cb0a812916d16c045985e72792d4
Vaishnav G M <132495879+VaishnavGM@users.noreply.github.com>
2023-07-14 12:22:52 +0530

    Add files via upload

---
+++ Stacksss.java
@@ -0,0 +1,19 @@
+package Vaish;
+import java.util.*;
+public class Stacksss {
+
+	public static void main(String[] args) {
+Stack s = new Stack();
+s.push(100);
+s.push(200);
+s.push(300);
+s.push(400);
+s.push(500);
+System.out.println(s);
+System.out.println(s.pop());
+System.out.println(s.peek());
+System.out.println(s.search(100));
+System.out.println(s.empty());
+	}
+
+}
